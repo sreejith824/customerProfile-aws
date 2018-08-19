298fe5c5502893007a276377e7b9435fe09c68f7
Sasidharan <AB33960@dnbnor.net>
2018-08-19 22:14:21 +0200

    fixing

--- src/main/java/com/tcs/mmisamples/customerprofile/CustomerProfileApplication.java
+++ src/main/java/com/tcs/mmisamples/customerprofile/CustomerProfileApplication.java
@@ -3,6 +3,7 @@ package com.tcs.mmisamples.customerprofile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * Created by SSasidharan on 2016/12/14.
@@ -14,6 +15,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories
 @SpringBootApplication
 public class CustomerProfileApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(CustomerProfileApplication.class, args);
