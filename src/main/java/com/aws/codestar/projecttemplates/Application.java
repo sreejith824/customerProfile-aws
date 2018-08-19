261fa30ef18c35b8f28ec8645d2e842178949729
Sasidharan <AB33960@dnbnor.net>
2018-08-19 22:00:09 +0200

    updating bootstrap

--- src/main/java/com/aws/codestar/projecttemplates/Application.java
+++ src/main/java/com/aws/codestar/projecttemplates/Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import com.tcs.mmisamples.customerprofile.CustomerProfileApplication;
 
 /** Simple class to start up the application.
  *
@@ -12,7 +14,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  *  @EnableAutoConfiguration
  *  @ComponentScan
  */
-@SpringBootApplication
+//@EnableJpaRepositories
+//@SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
     @Override
@@ -21,7 +24,7 @@ public class Application extends SpringBootServletInitializer {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(CustomerProfileApplication.class, args);
     }
 
 }
